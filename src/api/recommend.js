4480093ea3cceee18bbb9f0855f5acb0c75f605f
Tecode <283731869@qq.com>
2018-08-26 11:38:34 +0800

    跨域处理

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,6 +1,5 @@
 import jsonp from '../common/js/jsonp'
 import {commonParams, options} from './config'
-import axios from 'axios'
 
 export function getRecommend() {
  const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
@@ -26,13 +25,8 @@ export function getDiscList() {
     sortId: 5,
     needNewCode: 0,
     categoryId: 10000000,
-    rnd:Math.random(),
-    format:'json'
+    rnd:Math.random()
   })
-  return axios.get(url,{
-    params:data
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  })
+  return jsonp(url, data, options)
 }
 
